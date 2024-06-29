e25c5531075b4d10b5a17482689415d7e0e3a782
Sahidhusen Mukdiwala <shahidhusenmukdiwala1@gmail.com>
2024-06-29 12:03:31 +0530

    Add redux persist

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 export default function Home() {
   return (
     <div className='mt-3 mb-3 '>
+      <h1>Home</h1>
       {/* <Link to={'/create-listing'} className=' bg-green-700 text-white uppercase p-3 text-center rounded-lg hover:opacity-95'>Create Listing</Link> */}
     </div>
   )
