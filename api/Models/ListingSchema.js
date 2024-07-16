4dc71f427ca9c4439074aa2fc8e209cae518e879
Sahidhusen Mukdiwala <shahidhusenmukdiwala1@gmail.com>
2024-07-16 12:29:08 +0530

    Changes

--- api/Models/ListingSchema.js
+++ api/Models/ListingSchema.js
@@ -54,26 +54,10 @@ const ListingSchema = new mongoose.Schema({
         ref: 'Agent',
         required: true,
       },
-//      ratings: [{
-//     user: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: 'User',
-//     },
-//     rating: {
-//       type: Number,
-//       required: true,
-//       min: 1,
-//       max: 5,
-//     },
-//     comment: {
-//       type: String,
-//       default: '',
-//     },
-//     date: {
-//       type: Date,
-//       default: Date.now,
-//     },
-//   }],
+     reviews:{
+      type: mongoose.Types.ObjectId,
+      ref: 'Review',
+    }
 }, { timestamps: true });
 
 const Listing = mongoose.model('Listing', ListingSchema);
