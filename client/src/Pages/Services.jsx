cd14cd63034d6cafd8729faad974335527d998dd
Sahidhusen Mukdiwala <shahidhusenmukdiwala1@gmail.com>
2024-07-20 12:15:43 +0530

    add images in service page

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../assets/image/1.png'
-// import img2 from '../assets/image/2.jpeg'
+import img2 from '../assets/image/OIP.jpeg'
+import img3 from '../assets/image/download.jpeg'
 function Services() {
     return (
         <div className='Services mb-4'>
@@ -10,7 +11,7 @@ function Services() {
                 </div>
 
                 <div className="service-card  duration-[1s] cursor-pointer flex gap-3 justify-evenly flex-wrap">
-                    <div className="service-card-1 border p-10 shadow-lg border-b-2 border-b-red-900 rounded-lg hover:scale-105 hover:translate-y-[-10px] duration-[1s]">
+                    <div className="service-card-1 border p-10 shadow-lg border-b-2 border-b-red-900 rounded-lg hover:scale-105 hover:translate-y-[-10px] duration-[1s] hover:bg-red-900 hover:text-white">
                         <img src={img1} alt="cardImg" className=' mt-7 mx-auto w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] rounded-full' />
 
                         <div className="text-center mt-7">
@@ -18,16 +19,16 @@ function Services() {
                             <p className='font-semibold'>Property reviews from users help you <br /> make informed decisions.</p>
                         </div>
                     </div>
-                    <div className="service-card-1 border p-3 shadow-lg border-b-2 border-b-red-900 rounded-lg hover:scale-105 hover:translate-y-[-10px] duration-[1s]">
-                        <img src={img1} alt="cardImg" className='mt-7 mx-auto w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] rounded-full ' />
+                    <div className="service-card-1 border p-3 shadow-lg border-b-2 border-b-red-900 rounded-lg hover:scale-105 hover:translate-y-[-10px] duration-[1s] hover:bg-red-900 hover:text-white">
+                        <img src={img2} alt="cardImg" className='mt-7 mx-auto w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] rounded-full ' />
 
                         <div className="text-center mt-7">
                             <h3 className='font-bold text-xl'>Agent Support</h3>
                             <p className='font-semibold'>Agents can easily add properties and <br /> manage authorization, ensuring a <br /> smooth process.</p>
                         </div>
                     </div>
-                    <div className="service-card-1 border p-3 shadow-lg border-b-2 border-b-red-900 rounded-lg hover:scale-105 hover:translate-y-[-10px] duration-[1s]">
-                        <img src={img1} alt="cardImg" className='mt-7 mx-auto w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] rounded-full' />
+                    <div className="service-card-1 border p-3 shadow-lg border-b-2 border-b-red-900 rounded-lg hover:scale-105 hover:translate-y-[-10px] duration-[1s] hover:bg-red-900 hover:text-white">
+                        <img src={img3} alt="cardImg" className='mt-7 mx-auto w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] rounded-full' />
 
                         <div className="text-center mt-7">
                             <h3 className='font-bold text-xl'>Property Listings</h3>
